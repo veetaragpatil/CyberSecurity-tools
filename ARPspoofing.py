0b7012a153ab480a5f01e929f7125a5d8ad1204e
veetaragpatil <165647926+veetaragpatil@users.noreply.github.com>
2025-09-19 23:35:18 +0530

    Update ARPspoofing.py

--- ARPspoofing.py
+++ ARPspoofing.py
@@ -1,2 +1,3 @@
 #this the code of  ARPSpoofing tool where this used for attack like "man in the middle".
-#the command is arpspoof -i 
+#the command is for capturing packet *arpspoof -i [interface] -t [clientIP] [getwayIP]*
+#the command is for forwarding packet *arpspoof -i [interface] -t [getwayIP] [clientIP]*
